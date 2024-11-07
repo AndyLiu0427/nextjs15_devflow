d26104e1b4142dd9e0ac42dfa7f71ec6911a9e40
AndyLiu0427 <ay0933@gmail.com>
2024-11-07 22:23:47 +0800

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 const Home = () => (
-  <h1 className="h1-bold background-light850_dark100">Welcome to </h1>
+  <>
+    <h1 className="h1-bold">Welcome to </h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome to </h1>
+  </>
 );
 
 export default Home;
